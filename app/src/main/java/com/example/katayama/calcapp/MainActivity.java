b59abf57dcd5a5deedf3894071686b63a7e167d8
katayama <yasucom4646@gmail.com>
2016-12-14 03:14:45 +0900

    課題：四則計算アプリ

---
+++ app/src/main/java/com/example/katayama/calcapp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.katayama.calcapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v){
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        EditText editText1 = (EditText)findViewById(R.id.editText1);
+        EditText editText2 = (EditText)findViewById(R.id.editText2);
+        String number1 = editText1.getText().toString();
+        String number2 = editText2.getText().toString();
+        int result1 = Integer.parseInt(number1);
+        int result2 = Integer.parseInt(number2);
+        switch (v.getId()) {
+            case R.id.button1:
+                intent.putExtra("RESULT", result1 + result2);
+                break;
+            case R.id.button2:
+                intent.putExtra("RESULT", result1 - result2);
+                break;
+            case R.id.button3:
+                intent.putExtra("RESULT", result1 * result2);
+                break;
+            case R.id.button4:
+                intent.putExtra("RESULT", result1 / result2);
+                break;
+        }
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/example/katayama/calcapp/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.example.katayama.calcapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        int result = intent.getIntExtra("RESULT", 0);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(String.valueOf(result));
+
+    }
+}
